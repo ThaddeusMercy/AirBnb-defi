e30814d0b7df95bd17336fc6370f920ac9875879
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2022-12-25 14:57:08 +0530

    Update AirBnbToken.sol
    Token Supply Change

--- presalecorelib/Contract/AirBnbToken.sol
+++ presalecorelib/Contract/AirBnbToken.sol
@@ -693,7 +693,7 @@ contract AirBnbDefi is Context, IERC20, Ownable {
     address[] private _excluded;
    
     uint256 private constant MAX = ~uint256(0);
-    uint256 private _tTotal = 1000000000 * 10**6 * 10**9;
+    uint256 private _tTotal = 100000000 * 10**6 * 10**9;
     uint256 private _rTotal = (MAX - (MAX % _tTotal));
     uint256 private _tFeeTotal;
 
